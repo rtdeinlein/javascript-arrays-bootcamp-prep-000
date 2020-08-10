f90c60e54bc9619f699df19065d290fb1a0ba8e2
Reid T Deinlein <rtdeinlein5@gmail.com>
2020-08-10 17:33:13 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -29,5 +29,5 @@ function destructivelyRemoveElementFromEndOfArray (chocolateBars){
   return chocolateBars;
 }
 function removeELementFromEndOfArray (chocolateBars){
-return chocolateBars.slice(0, chocolateBars.length - 2);
+return chocolateBars.slice(0, chocolateBars.length - 1);
 }
