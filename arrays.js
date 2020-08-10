a2481698459970d027452593d941d76d1c7cba6c
Reid T Deinlein <rtdeinlein5@gmail.com>
2020-08-10 17:00:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,16 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+
 function addElementToBeginningOfArray(chocolateBars, candyString){
   return [candyString, ...chocolateBars];
 }
-function destructivelyAddElementToBeginningOfArray = [chocolateBars, candyString]{
-  
+function destructivelyAddElementToBeginningOfArray (chocolateBars, candyString){
+  chocolateBars.unshift(candyString)
+  return chocolateBars;
+}
+function addElementToEndOfArray (chocolateBars, candyString){
+  return [...chocolateBars, candyString];
+}
+function destructivelyAddElementToEndOfArray (chocolateBars, candyString){
+  chocolateBars.push(candyString)
+  return chocolateBars;
 }
