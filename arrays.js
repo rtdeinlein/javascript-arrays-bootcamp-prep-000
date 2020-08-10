d88e20af06781c0772b3e5c711fcd945c8b3e92e
Reid T Deinlein <rtdeinlein5@gmail.com>
2020-08-10 17:03:01 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,3 +14,7 @@ function destructivelyAddElementToEndOfArray (chocolateBars, candyString){
   chocolateBars.push(candyString)
   return chocolateBars;
 }
+function accessElementInArray (array, index){
+  return array[index]
+}
+funtion destructivelyRemoveElementFromBeginningOfArray ()
